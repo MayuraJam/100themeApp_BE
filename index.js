f71bdf265b56812b6150f4bc8c1a55b51096c86e
Mayura <mayura.jam.mai@gmail.com>
2026-06-29 00:21:49 +0700

    ทำการจัดระเบียบ folder structure ใหม่ โดยแบ่งเป็น controller router service และทำการทดสอบด้วย star system service

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const {
   insertNewTheme,
   getAllThemeForDropdown,
   updateTheme,
-  getThemeById
+  getThemeById,
 } = require("./controller/theme.controller");
+const starSystemRoute = require("./router/starSystem.route");
 const cors = require("cors");
 const app = express();
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
 // const swaggerDocument = require('./swagger.json');
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const axios = require("axios");
 
 require("dotenv").config();
 
@@ -46,17 +50,50 @@ app.get("/index", (req, res) => {
   res.send("Hello my new express");
 });
 
+//for google login
+app.get("/api/google-login", async (req, res) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        message: "Authorization header missing or malformed",
+        success: false,
+      });
+    }
+    const token = authHeader.split(" ")[1];
+
+    const googleResponse = await axios.get(
+      "https://www.googleapis.com/oauth2/v3/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    const payload = googleResponse.data;
+    res.json({ success: true, message: "Login successful", data: payload });
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({
+      message: "Invalid token",
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
 //get all theme
 
 app.get("/api/theme", async (req, res) => {
   try {
     const getTheme = await getAllTheme();
     // console.log("response:",getTheme);
-    res.json(getTheme);
+    res.json({ success: true, message: "ดึงข้อมูลสำเร็จ", data: getTheme });
   } catch (error) {
     console.error(error);
     res.status(500).json({
-      message: "เกิดข้อผืดผลาดในการดึงข้อมูล",
+      message: "เกิดข้อผิดผลาดในการดึงข้อมูล",
       success: false,
       error: error.message,
     });
@@ -67,11 +104,11 @@ app.get(`/api/theme/:id`, async (req, res) => {
   const reqParam = req.params.id;
   try {
     const getTheme = await getThemeById(reqParam);
-    res.json(getTheme);
+    res.json({ success: true, message: "ดึงข้อมูลสำเร็จ", data: getTheme });
   } catch (error) {
     console.error(error);
     res.status(500).json({
-      message: "เกิดข้อผืดผลาดในการดึงข้อมูล",
+      message: "เกิดข้อผิดผลาดในการดึงข้อมูล",
       success: false,
       error: error.message,
     });
@@ -81,11 +118,11 @@ app.get(`/api/theme/:id`, async (req, res) => {
 app.get("/api/theme/ForDropdown", async (req, res) => {
   try {
     const getTheme = await getAllThemeForDropdown();
-    res.json(getTheme);
+    res.json({ success: true, message: "ดึงข้อมูลสำเร็จ", data: getTheme });
   } catch (error) {
     console.error(error);
     res.status(500).json({
-      message: "เกิดข้อผืดผลาดในการดึงข้อมูล",
+      message: "เกิดข้อผิดผลาดในการดึงข้อมูล",
       success: false,
       error: error.message,
     });
@@ -97,7 +134,7 @@ app.post("/api/theme", async (req, res) => {
     const reqBody = req.body;
     const { themeName, color100, color75, color25, color20, colorTone } =
       reqBody;
-      console.log("insert data",reqBody);
+    console.log("insert data", reqBody);
     const resData = await insertNewTheme(
       themeName,
       color100,
@@ -110,27 +147,27 @@ app.post("/api/theme", async (req, res) => {
     res.status(200).json({
       message: "เพิ่มข้อมูลสำเร็จ",
       success: true,
-      data: resData 
+      data: resData,
     });
-
   } catch (error) {
     console.error(error);
     res.status(500).json({
-      message: "เกิดข้อผืดผลาดในการจัดการข้อมูล",
+      message: "เกิดข้อผิดผลาดในการจัดการข้อมูล",
       success: false,
       error: error.message,
     });
   }
 });
 
-app.put("/api/theme", async (req, res) => {
+app.put(`/api/theme/:id`, async (req, res) => {
+  const reqParam = req.params.id;
   try {
     const reqBody = req.body;
-    const { themeName, color100, color75, color25, color20, colorTone ,id} =
+    const { themeName, color100, color75, color25, color20, colorTone } =
       reqBody;
-      console.log("insert data",reqBody);
+    console.log("insert data", reqBody);
     const resData = await updateTheme(
-      id,
+      reqParam,
       themeName,
       color100,
       color75,
@@ -142,18 +179,19 @@ app.put("/api/theme", async (req, res) => {
     res.status(200).json({
       message: "แก้ไขข้อมูลสำเร็จ",
       success: true,
-      data: resData 
+      data: resData,
     });
-
   } catch (error) {
     console.error(error);
     res.status(500).json({
-      message: "เกิดข้อผืดผลาดในการจัดการข้อมูล",
+      message: "เกิดข้อผิดผลาดในการจัดการข้อมูล",
       success: false,
       error: error.message,
     });
   }
 });
+
+app.use("/api/star-system", starSystemRoute);
 
 app.listen(port, () => {
   console.log("server is running on port : ", port);
